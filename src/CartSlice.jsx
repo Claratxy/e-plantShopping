9852ff56d0bf2fc8bbf07e29c11fc79f3337b6d0
Claratxy <xinyue274404@gmail.com>
2025-07-29 05:30:07 -0400

    Fix cost calculation and button function on cart page.

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,36 +4,47 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    numOfItems: 0 // Number of items multiplied by their quantity
   },
+
   reducers: {
-    // Add item to cart
     addItem: (state, action) => {
-        const { name, image, cost } = action.payload;
-        const existingItem = state.items.find(item => item.name === name);
-        if (existingItem) {
-          existingItem.quantity++;
-        } else {
-          state.items.push({ name, image, cost, quantity: 1 });
-        }
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+
+      if (existingItem) {
+        // In existing items, quantity is already added as property
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+
+      state.numOfItems += 1;
     },
-  
-    // Remove item from cart
+
     removeItem: (state, action) => {
-        const itemNameToRemove = action.payload;
-        state.items = state.items.filter(item => item.name !== itemNameToRemove);
+      const { name, quantity } = action.payload;
+      state.items = state.items.filter(item => item.name !== name);
+      state.numOfItems -= quantity;
+
+      // Just to be sure... I hate negative numbers
+      if (state.numOfItems < 0) {
+        state.numOfItems = 0;
+      }
     },
-  
-    // date quantity of item
+
     updateQuantity: (state, action) => {
-        const { name, quantity } = action.payload;
-        const itemToUpdate = state.items.find(item => item.name === name);
-        if (itemToUpdate) {
-          itemToUpdate.quantity = quantity;
-        }
+      const { name, quantity } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+
+      if (existingItem) {
+        const differenceQuantity = quantity - existingItem.quantity;
+        state.numOfItems += differenceQuantity;
+        existingItem.quantity = quantity;
+      }
     },
   },
 });
-  
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
